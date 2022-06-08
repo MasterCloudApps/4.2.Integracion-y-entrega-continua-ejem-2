b8d52eabfa5ed4791c1a19ff6f7bfafcb7aeffaf
Maes95 <michel.maes95@gmail.com>
2022-06-08 19:15:31 +0200

    Upload all

---
+++ src/test/java/es/codeurjc/tablonanuncios/AnuncioTest.java
@@ -0,0 +1,73 @@
+package es.codeurjc.tablonanuncios;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import es.codeurjc.test.tablonanuncios.Application;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AnuncioTest {
+
+	@LocalServerPort
+	int port;
+
+	WebDriver driver;
+	
+	@BeforeAll
+	public static void setupClass() {
+		WebDriverManager.chromedriver().setup();
+	}
+	
+	@BeforeEach
+	public void setup() {
+		ChromeOptions options = new ChromeOptions();
+		// options.addArguments("--headless");
+		driver = new ChromeDriver(options);
+	}
+	
+	@AfterEach
+	public void teardown() {
+		if(driver != null) {
+			driver.quit();
+		}
+	}
+	
+	@Test
+	public void createAnuncio() throws InterruptedException {
+		driver.get("http://localhost:"+this.port+"/");
+
+		Thread.sleep(2000);
+		
+		driver.findElement(By.linkText("Nuevo anuncio")).click();
+
+		Thread.sleep(2000);
+		
+		driver.findElement(By.name("nombre")).sendKeys("Michel");
+		driver.findElement(By.name("asunto")).sendKeys("Vendo moto roja");
+		driver.findElement(By.name("comentario")).sendKeys("Un comentario muy largo...");
+		
+		Thread.sleep(2000);
+
+		driver.findElement(By.id("enviar")).click();
+		
+		Thread.sleep(2000);
+
+		driver.findElement(By.linkText("Volver al tablón")).click();
+		
+		Thread.sleep(2000);
+
+		assertNotNull(driver.findElement(By.partialLinkText("Vendo moto roja")));
+	}
+
+}
